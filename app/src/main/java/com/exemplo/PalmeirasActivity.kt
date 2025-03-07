fa20b10dace1927a4af457c6c4d752f79789b4c2
julia_ribeiro <113191131+juapparecido@users.noreply.github.com>
2025-03-07 12:54:11 -0300

    Update PalmeirasActivity.kt

--- app/src/main/java/com/exemplo/PalmeirasActivity.kt
+++ app/src/main/java/com/exemplo/PalmeirasActivity.kt
@@ -1,1 +1,11 @@
 
+package com.example.diogoddc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PalmeirasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
