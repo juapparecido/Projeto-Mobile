389c18bcfa363ad8e1adcd8f43e6beed0c1e8337
julia_ribeiro <113191131+juapparecido@users.noreply.github.com>
2025-03-07 12:57:07 -0300

    Create FluminenseAtividade.kt

---
+++ app/src/main/java/com/exemplo/FluminenseAtividade.kt
@@ -0,0 +1,10 @@
+package com.example.diogoddc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FluminenseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
