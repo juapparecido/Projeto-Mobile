141b6ad80ebb7c780c4781f16438c353b8912c8b
julia_ribeiro <113191131+juapparecido@users.noreply.github.com>
2025-03-07 12:59:36 -0300

    Create AtleticoMGActivity.kt

---
+++ app/src/main/java/com/exemplo/AtleticoMGActivity.kt
@@ -0,0 +1,10 @@
+package com.example.diogoddc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AtleticoMGActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
